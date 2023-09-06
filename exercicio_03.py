78b6759d7ea9e0fe09f72143ee616e0e8e676e5f
Bernardo Guerra <bernardoguerra93@gmail.com>
2023-09-06 11:18:05 -0300

    Fiz o segundo exercício

---
+++ exercicio_03.py
@@ -0,0 +1,9 @@
+#caucular média
+
+print("vamos ver a media do aluno")
+num1=float(input("insira a primeira nota: "))
+num2=float(input("agora, insira a segunda nota: "))
+
+media = (num1 + num2)/2
+media=print("a média do aluno é: ", media)
+
